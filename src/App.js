8ad5ad830b1bdc4e2541ea3a5b7e0076fb525fbc
Aditya Pratap Singh <60227945+AdityaPSingh3@users.noreply.github.com>
2024-02-25 11:36:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import FullNameForm from "./FullNameForm";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FullNameForm />
+    </div>
+  );
+}
---
+++ src/FullNameForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const FullNameForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && lastName) {
+      setFullName(`${firstName} ${lastName}`);
+      setIsSubmitted(true);
+    }
+  };
+
+  return (
+    <div style={{ marginLeft: "10px" }}>
+      <form onSubmit={handleSubmit} style={{ display: "inline-block" }}>
+        <label>
+          First Name:
+          <br />
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <br />
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {isSubmitted && (
+        <p style={{ display: "inline-block", marginLeft: "10px" }}>
+          Full Name: {fullName}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default FullNameForm;
